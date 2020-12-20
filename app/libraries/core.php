d5cb28e70e6328e2e4ac5ae88d807a49582f923f
vkrouza <krouza.v@gmail.com>
2020-12-20 17:04:06 +0100

    initializing controller for pages

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -12,11 +12,37 @@ class Core
 
     public function __construct()
     {
-        $this->getUrl();
+        //print_r($this->getUrl());
+
+        $url = $this->getUrl();
+
+        //Look in controllers for first value (index)
+        if (file_exists('../app/controllers/' . ucwords($url[0]) . 'php')) {
+            //If exists, set as controller
+            $this->currentController = ucwords($url[0]);
+            //Unset 0 index
+            unset($url[0]);
+        }
+        //require the controller
+        require_once '../app/controllers/' . $this->currentController . '.php';
+
+        // Instatiate controller class
+        $this->currentController = new $this->currentController;
     }
 
     public function getUrl()
     {
-        echo $_GET['url'];
+        if (isset($_GET['url'])) {
+            //stript URL of any "/" on the end of the string
+            $url = rtrim($_GET['url'], '/');
+            //sanitaze URL 
+            //FILTER_SANITIZE_URL removes:
+            //Remove all characters except letters, digits 
+            //and $-_.+!*'(),{}|\\^~[]`<>#%";/?:@&=.
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            //break url into array by /
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
