8b6f3b7e1ef03f53cd376bf4c431ce58984e36f7
vkrouza <krouza.v@gmail.com>
2020-12-28 20:22:55 +0100

    cleaning up

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,7 +1,3 @@
 <?php require APPROOT . "/views/inc/header.php"; ?>
-<h1> WELCOME </h1>
-<ul> <?php foreach ($data['posts'] as $post) : ?>
-    <li> <?php echo $post->title; ?> </li>
-    <?php endforeach; ?>
-</ul>
+<h1><?php echo $data['title']; ?> </h1>
 <?php require APPROOT . "/views/inc/footer.php"; ?>
