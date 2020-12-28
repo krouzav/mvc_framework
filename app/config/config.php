676486f7ae8252ca79d52386820736c0cc88f45b
vkrouza <krouza.v@gmail.com>
2020-12-28 20:18:02 +0100

    set up pdo

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,11 @@
 <?php
+//DB param
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'mvc');
+
+
 // app root
 
 define('APPROOT', dirname(dirname(__FILE__)));
